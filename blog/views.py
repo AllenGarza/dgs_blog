e54b04c6183f78d386316874605a9f485ca0fd2c
allen <allenfg86@gmail.com>
2021-04-20 12:55:09 -0500

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+# Views take a web request and creates a web response. response can be HTML, XML, 404, etc.
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
