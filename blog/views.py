fe8ee7bea8e8a5ffa6054054f8f6b944c49f5ea2
allen <allenfg86@gmail.com>
2021-04-21 08:57:20 -0500

    Display posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 # Views take a web request and creates a web response. response can be HTML, XML, 404, etc.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
